54795b6c4d23734736c97ed6f48f860b62d56810
Christian Treffs <ctreffs@gmail.com>
2020-03-25 13:23:44 +0100

    Swift 5.2 package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SwiftSimctl",
     platforms: [
         .iOS(.v11),
+        .tvOS(.v11),
         .macOS(.v10_11)
     ],
     products: [
@@ -16,9 +17,9 @@ let package = Package(
                  targets: ["Simctl"])
     ],
     dependencies: [
-        .package(url: "https://github.com/httpswift/swifter.git", from: "1.4.7"),
+        .package(name: "swift-log", url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+        .package(name: "Swifter", url: "https://github.com/httpswift/swifter.git", from: "1.4.7"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.2"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0")
     ],
     targets: [
@@ -26,15 +27,17 @@ let package = Package(
         .target(
             name: "SimctlCLI",
             dependencies: [
+                .byName(name: "ShellOut"),
+                .byName(name: "SimctlShared"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Swifter"),
-                .product(name: "Logging"),
-                .product(name: "ShellOut"),
-                .byName(name: "SimctlShared")
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Swifter", package: "Swifter")
         ]),
         .target(name: "Simctl",
-                dependencies: ["Logging",
-                               "SimctlShared"]),
+                dependencies: [
+                    .byName(name: "SimctlShared"),
+                    .product(name: "Logging", package: "swift-log")
+        ]),
         .testTarget(name: "SimctlTests",
                     dependencies: ["Simctl"])
         
