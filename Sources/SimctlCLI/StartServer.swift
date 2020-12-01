f80c61be7a497573ee7798dc4ebd800d32245795
Thomas Counsell <tamc@greenonblack.com>
2020-12-01 14:05:06 +0000

    Add --verbose flag to start-server subcommand
    This prints out the exact command that is being run and the response

--- Sources/SimctlCLI/StartServer.swift
+++ Sources/SimctlCLI/StartServer.swift
@@ -18,43 +18,47 @@ struct StartServer: ParsableCommand {
     @Option(name: .shortAndLong, help: "The port to listen on")
     var port: SimctlShared.Port = Port(8080)
     
+    @Flag(name: .shortAndLong, help: "Show the commands received and the responses sent")
+    var verbose = false
+    
     mutating func run() throws {
         let server = SimctlServer()
+        let v = verbose
         
         server.onPushNotification { deviceId, bundleId, pushContent -> Result<String, Swift.Error> in
-            return runCommand(.simctlPush(to: deviceId, pushContent: pushContent, bundleIdentifier: bundleId))
+            return runCommand(.simctlPush(to: deviceId, pushContent: pushContent, bundleIdentifier: bundleId), verbose: v)
         }
 
         server.onPrivacy { deviceId, bundleId, action, service -> Result<String, Swift.Error> in
-            return runCommand(.simctlPrivacy(action, permissionsFor: service, on: deviceId, bundleIdentifier: bundleId))
+            return runCommand(.simctlPrivacy(action, permissionsFor: service, on: deviceId, bundleIdentifier: bundleId), verbose: v)
         }
 
         server.onRename { deviceId, _, newName -> Result<String, Swift.Error> in
-            return runCommand(.simctlRename(device: deviceId, to: newName))
+            return runCommand(.simctlRename(device: deviceId, to: newName), verbose: v)
         }
 
         server.onTerminateApp { deviceId, _, appBundleId -> Result<String, Swift.Error> in
-            return runCommand( .simctlTerminateApp(device: deviceId, appBundleIdentifier: appBundleId))
+            return runCommand( .simctlTerminateApp(device: deviceId, appBundleIdentifier: appBundleId), verbose: v)
         }
 
         server.onSetDeviceAppearance { deviceId, _, appearance -> Result<String, Swift.Error> in
-            return runCommand(.simctlSetUI(appearance: appearance, on: deviceId))
+            return runCommand(.simctlSetUI(appearance: appearance, on: deviceId), verbose: v)
         }
 
         server.onTriggerICloudSync { deviceId, _ -> Result<String, Swift.Error> in
-            return runCommand(.simctlTriggerICloudSync(device: deviceId))
+            return runCommand(.simctlTriggerICloudSync(device: deviceId), verbose: v)
         }
 
         server.onUninstallApp { deviceId, _, appBundleId -> Result<String, Swift.Error> in
-            return runCommand(.simctlUninstallApp(device: deviceId, appBundleIdentifier: appBundleId))
+            return runCommand(.simctlUninstallApp(device: deviceId, appBundleIdentifier: appBundleId), verbose: v)
         }
 
         server.onSetStatusBarOverride { deviceId, _, overrides -> Result<String, Swift.Error> in
-            return runCommand(.simctlSetStatusBarOverrides(device: deviceId, overrides: overrides))
+            return runCommand(.simctlSetStatusBarOverrides(device: deviceId, overrides: overrides), verbose: v)
         }
 
         server.onClearStatusBarOverrides { deviceId, _ -> Result<String, Swift.Error> in
-            return runCommand(.simctlClearStatusBarOverrides(device: deviceId))
+            return runCommand(.simctlClearStatusBarOverrides(device: deviceId), verbose: v)
         }
         
         server.startServer(on: port)
@@ -62,11 +66,20 @@ struct StartServer: ParsableCommand {
     
 }
 
-private func runCommand(_ cmd: ShellOutCommand) -> Result<String, Swift.Error> {
+private func runCommand(_ cmd: ShellOutCommand, verbose: Bool = false) -> Result<String, Swift.Error> {
+    if verbose {
+        print("Command: \(cmd.string)")
+    }
     do {
         let output: String = try shellOut(to: cmd)
+        if verbose {
+            print("Success: \(output)")
+        }
         return .success(output)
     } catch {
+        if verbose {
+            print("Failure: \(error)")
+        }
         return .failure(error)
     }
 }
